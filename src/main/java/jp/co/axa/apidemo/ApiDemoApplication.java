c366846e0c249b84b8363ad8dd2d614eb579266b
zyd <zydmayday@gmail.com>
2021-11-12 23:34:16 +0900

    Format code and add FIXME

--- src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
+++ src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
@@ -8,8 +8,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class ApiDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiDemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ApiDemoApplication.class, args);
+  }
 
 }
--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -11,44 +11,43 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class EmployeeController {
 
-    @Autowired
-    private EmployeeService employeeService;
+  @Autowired
+  private EmployeeService employeeService;
 
-    public void setEmployeeService(EmployeeService employeeService) {
-        this.employeeService = employeeService;
+  public void setEmployeeService(EmployeeService employeeService) {
+    this.employeeService = employeeService;
+  }
+
+  @GetMapping("/employees")
+  public List<Employee> getEmployees() {
+    return employeeService.retrieveEmployees();
+  }
+
+  @GetMapping("/employees/{employeeId}")
+  public Employee getEmployee(@PathVariable(name = "employeeId") Long employeeId) {
+    return employeeService.getEmployee(employeeId);
+  }
+
+  @PostMapping("/employees")
+  public void saveEmployee(Employee employee) {
+    employeeService.saveEmployee(employee);
+    System.out.println("Employee Saved Successfully");
+  }
+
+  @DeleteMapping("/employees/{employeeId}")
+  public void deleteEmployee(@PathVariable(name = "employeeId") Long employeeId) {
+    employeeService.deleteEmployee(employeeId);
+    System.out.println("Employee Deleted Successfully");
+  }
+
+  @PutMapping("/employees/{employeeId}")
+  public void updateEmployee(@RequestBody Employee employee,
+      @PathVariable(name = "employeeId") Long employeeId) {
+    Employee emp = employeeService.getEmployee(employeeId);
+    if (emp != null) {
+      employeeService.updateEmployee(employee);
     }
 
-    @GetMapping("/employees")
-    public List<Employee> getEmployees() {
-        List<Employee> employees = employeeService.retrieveEmployees();
-        return employees;
-    }
-
-    @GetMapping("/employees/{employeeId}")
-    public Employee getEmployee(@PathVariable(name="employeeId")Long employeeId) {
-        return employeeService.getEmployee(employeeId);
-    }
-
-    @PostMapping("/employees")
-    public void saveEmployee(Employee employee){
-        employeeService.saveEmployee(employee);
-        System.out.println("Employee Saved Successfully");
-    }
-
-    @DeleteMapping("/employees/{employeeId}")
-    public void deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
-        employeeService.deleteEmployee(employeeId);
-        System.out.println("Employee Deleted Successfully");
-    }
-
-    @PutMapping("/employees/{employeeId}")
-    public void updateEmployee(@RequestBody Employee employee,
-                               @PathVariable(name="employeeId")Long employeeId){
-        Employee emp = employeeService.getEmployee(employeeId);
-        if(emp != null){
-            employeeService.updateEmployee(employee);
-        }
-
-    }
+  }
 
 }
