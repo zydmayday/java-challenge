87a5a8dd566fe4d0bc691c3c0d3648ee2ab2c25c
zyd <zydmayday@gmail.com>
2021-11-18 22:34:20 +0900

    Fix github workflows error

--- src/main/java/jp/co/axa/apidemo/redis/RedisConfig.java
+++ src/main/java/jp/co/axa/apidemo/redis/RedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
@@ -18,6 +19,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Profile("dev")
 @EnableCaching
 @Configuration
 public class RedisConfig extends CachingConfigurerSupport {
